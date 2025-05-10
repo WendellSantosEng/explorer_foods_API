cb313c0271dd5a67685ff501db2b97d636d32162
Wendell Santos <100713532+WendellSantosEng@users.noreply.github.com>
2025-05-10 10:59:25 -0300

    Update sessionsController.js

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,35 +1,43 @@
-const knex = require ('../database/knex');
-const appError = require('../utils/appError');
+const knex = require('../database/knex');
+const AppError = require('../utils/appError');
 const AuthConfig = require('../configs/Auth');
 
-const {compare} = require('bcrypt');
-const {sign} = require('jsonwebtoken')
-
+const { compare } = require('bcrypt');
+const { sign } = require('jsonwebtoken');
 
 class SessionsController {
-  async create(request, response){
-    const {email, password} = request.body;
+  async create(request, response) {
+    const { email, password } = request.body;
 
-    const user = await knex("users").where({email}).first();
+    const user = await knex("users").where({ email }).first();
 
-    if(!user) {
-      throw new appError("Email e/ou senha incorretos!", 401)
+    if (!user) {
+      console.warn(`Tentativa de login com e-mail inválido: ${email}`);
+      throw new AppError("As credenciais fornecidas estão incorretas.", 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
-    if(!passwordMatched){
-      throw new appError("Email e/ou senha incorretos!", 401)
+    if (!passwordMatched) {
+      console.warn(`Senha incorreta para o e-mail: ${email}`);
+      throw new AppError("As credenciais fornecidas estão incorretas.", 401);
     }
 
-    const {expiresIn, secret} = AuthConfig.jwt
+    const { expiresIn, secret } = AuthConfig.jwt;
     const token = sign({}, secret, {
       subject: String(user.id),
-      expiresIn
-    })
+      expiresIn,
+    });
 
-    return response.status(201).json({user, token});
+    // Retorna apenas dados essenciais do usuário
+    const userResponse = {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    };
+
+    return response.status(201).json({ user: userResponse, token });
   }
-} 
+}
 
 module.exports = SessionsController;
